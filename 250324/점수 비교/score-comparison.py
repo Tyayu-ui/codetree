5a985cafdae30d5e67a1795ef0ff79ba24acd551
Tyayu-ui <rhdcor23@naver.com>
2025-03-24 15:55:53 +0900

    Title: 점수 비교, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 250324/점수 비교/score-comparison.py
+++ 250324/점수 비교/score-comparison.py
@@ -1,7 +1,7 @@
 mathA, englishA = map(int, input().split())
 mathB, englishB = map(int, input().split())
 
-if mathA >= mathB and englishA >= englishB:
+if mathA > mathB and englishA > englishB:
     print("1")
 else:
     print("0")
