9318d29f8463f20032dc0bf1f487338449dec61a
Tyayu-ui <rhdcor23@naver.com>
2025-03-31 17:13:37 +0900

    Title: a로 채워넣기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250331/a로 채워넣기/filling-with-a.py
+++ 250331/a로 채워넣기/filling-with-a.py
@@ -1,5 +1,14 @@
-word = input()
+def replace_second_chars(s):
+    if len(s) < 2:
+        return s  # 문자열 길이가 2 미만이면 그대로 반환
 
-word1=word.replace(word[1], 'a',1)
-word2=word1.replace(word[-2],'a',-1)
-print(word2)
+    # 앞에서 두 번째 문자와 뒤에서 두 번째 문자 'a'로 변경
+    s_list = list(s)
+    s_list[1] = 'a'
+    s_list[-2] = 'a'
+
+    return "".join(s_list)
+
+# 테스트
+s = input().strip()
+print(replace_second_chars(s))
