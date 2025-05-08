fb56f4a8ebc0a7cc8003b20ba6baf79767b9f06f
Tyayu-ui <rhdcor23@naver.com>
2025-05-08 16:50:17 +0900

    Title: 두 개의 격자 비교하기, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250508/두 개의 격자 비교하기/compare-two-grid.py
+++ 250508/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,6 +1,6 @@
 row, col = map(int, input().split())
-arr1 = [list(map(int, input().split())) for _ in range(col)]
-arr2 = [list(map(int, input().split())) for _ in range(col)]
+arr1 = [list(map(int, input().split())) for _ in range(row)]
+arr2 = [list(map(int, input().split())) for _ in range(row)]
 arr3 = [[0 for _ in range(col)] for _ in range(row)]
 
 for i in range(row):
