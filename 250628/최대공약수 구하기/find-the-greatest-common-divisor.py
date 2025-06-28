08720125b4f51bf36f1e2266bf1d738e8316f6c6
Tyayu-ui <rhdcor23@naver.com>
2025-06-28 14:24:23 +0900

    Title: 최대공약수 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250628/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250628/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 num = 1
 result = 1
-for i in range(1, max(n, m)):
+for i in range(1, max(n, m)+1):
     if n % num == 0 and m % num == 0:
         result = num
     num+=1
