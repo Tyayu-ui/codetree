3bb9e6e31ee6dba294ef87c1447a3b3c4de5fb69
Tyayu-ui <rhdcor23@naver.com>
2025-10-26 13:48:22 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251026/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double num = 0.0;
+    cin >> num;
+    cout << fixed;
+    cout.precision(2);
+    cout << num;
+    return 0;
+}
